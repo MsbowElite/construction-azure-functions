d9c40fe2bbc08f67ddef139a0e9f970606a09a69
Lucas R Sena <msbowelite@gmail.com>
2022-05-29 20:58:35 -0300

    Get code

---
+++ FunctionAppCRUD/ConstructionFunctions.GetById.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppCRUD
+{
+    public partial class ConstructionFunctions
+    {
+        [Function("ConstructionFunctionsGetById")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = _baseRoute+"/{id}")] HttpRequest req, string id)
+        {
+            _logger.LogInformation($"{nameof(ConstructionFunctions)} function processed a request.");
+
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            var construction =
+                await _constructionDataStore.GetByIdAsync(id);
+
+            if (construction == null) return new NotFoundResult();
+
+            return new OkObjectResult(construction);
+        }
+    }
+}
